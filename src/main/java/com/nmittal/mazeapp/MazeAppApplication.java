9adae8c0b7a5352d2df479f10857c1c385bac832
Narain Mittal <mittalnarain123@gmail.com>
2018-02-11 09:29:10 -0800

    Initial Commit with Spring Boot app

---
+++ src/main/java/com/nmittal/mazeapp/MazeAppApplication.java
@@ -0,0 +1,12 @@
+package com.nmittal.mazeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MazeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MazeAppApplication.class, args);
+	}
+}
