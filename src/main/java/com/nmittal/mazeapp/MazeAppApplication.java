6d6145c25f0d44aab48d61df9226e8e648ccec0a
Narain Mittal <mittalnarain123@gmail.com>
2018-03-13 22:33:59 -0700

    Enable CORS

--- src/main/java/com/nmittal/mazeapp/MazeAppApplication.java
+++ src/main/java/com/nmittal/mazeapp/MazeAppApplication.java
@@ -2,6 +2,10 @@ package com.nmittal.mazeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class MazeAppApplication {
@@ -9,4 +13,14 @@ public class MazeAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MazeAppApplication.class, args);
 	}
+	
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+            	registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
 }
